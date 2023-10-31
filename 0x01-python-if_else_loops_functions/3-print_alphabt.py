bc2c80c91b429d42f673e0f668f9db83062b8e43
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-10-31 18:36:23 +0100

    add

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('q') and c != ord('e'):
-        print(chr(c), end='')
+for letter in range(97, 123):
+    if (letter == 101) or (letter == 113):
+        continue
+    print(chr(letter).format(), end="")
+
