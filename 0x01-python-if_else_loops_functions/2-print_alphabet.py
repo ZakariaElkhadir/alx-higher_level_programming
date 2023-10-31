a49a84060995f741dd24b9325d997008dee6f7f3
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-10-31 18:41:23 +0100

    add

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for let in range(ord('a'), ord('b') + 1):
-    print(chr(c).format(let), end="")
+for let in range(97, 123):
+    print(chr(let).format(), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,3 @@ for letter in range(97, 123):
     if (letter == 101) or (letter == 113):
         continue
     print(chr(letter).format(), end="")
-
