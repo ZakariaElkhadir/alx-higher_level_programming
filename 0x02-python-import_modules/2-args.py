7d7fb80b021bad0da3edb94a6ad16fa6b8dd8366
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-03 00:27:05 +0100

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "if __name__ == "__main__":
+if __name__ == "__main__":
     from sys import argv
     i = 1
-    args = len(argv) -1
+    args = len(argv) - 1
 
-    print(f"{args} {"argument" if args == 1 else "argument"}", end="")
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
     print(f"{'.' if args == 0 else ':'}")
     while (i <= args):
         print(f"{i}: {argv[i]}")
