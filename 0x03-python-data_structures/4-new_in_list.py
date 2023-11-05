1fe60adcf3623d7d5291887a6cdcb766995e0ee1
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-05 10:14:38 +0100

    corrected

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    
+
     Sec_list = my_list[:]
-    
+
     if 0 <= idx < len(my_list):
         Sec_list[idx] = element
     return Sec_list
