f130316d737375e188004707ceb4d863b89db045
Parrot Live user <user@localhost.localdomain>
2023-11-05 12:41:13 +0000

    add

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list) == 0:
-    if len (my_list) == 0:
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
         return ("None")
-    s = my_list[0]
+    x = my_list[0]
     for i in my_list:
-        if i > s:
-            s = i
-    return (s)
+        if i > x:
+            x = i
+    return (x)
