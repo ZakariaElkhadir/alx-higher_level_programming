796056d50e4f9f44db55ae79256040fac2aae8b9
Parrot Live user <user@localhost.localdomain>
2023-11-06 14:28:43 +0000

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list = my_list[:]
+    for count, i in enumerate(my_list):
+        if i % 2 == 0:
+            list[count] = True
+        else:
+            list[count] = False
+    return list
