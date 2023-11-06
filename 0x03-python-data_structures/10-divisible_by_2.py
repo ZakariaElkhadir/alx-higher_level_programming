d1073c41b8b68b84b86e123a2ee502fa3cf52874
Parrot Live user <user@localhost.localdomain>
2023-11-06 15:12:12 +0000

    edit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    list = my_list[:]
+    blist = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
-            list[count] = True
+            blist[count] = True
         else:
-            list[count] = False
-    return list
+            blist[count] = False
+    return(blist)
