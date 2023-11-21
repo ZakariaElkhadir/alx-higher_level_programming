83fa705f61fc9cfea2be22aac8baabe5c28bbaec
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-21 15:29:49 +0100

    edit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Square module"""
+
+
 class Square:
     """Private instance attribute for size"""
     def __init__(self, size=0) -> None:
         self.__size = size
         """set if else statment"""
         if type(size) != int:
-             raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
