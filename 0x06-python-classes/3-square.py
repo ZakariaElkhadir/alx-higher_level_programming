6f38e45788a5e1b89f639284f4b8f042068929fd
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-21 16:22:01 +0100

    edit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """Square module"""
 
+
 class Square:
-    """"""
+    """Private instance attribute"""
     def __init__(self, size=0) -> None:
         self.__size = size
+        """if else statment"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+        """area function with parameter(self)"""
     def area(self):
-        
-          return self.__size ** 2
+        """return area value"""
+        return self.__size ** 2
