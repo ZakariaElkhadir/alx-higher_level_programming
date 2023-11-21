93890fcf8e9e41c6e9129a455e08d20c254742a8
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-21 15:26:27 +0100

    add

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module"""
+class Square:
+    """Private instance attribute for size"""
+    def __init__(self, size=0) -> None:
+        self.__size = size
+        """set if else statment"""
+        if type(size) != int:
+             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
