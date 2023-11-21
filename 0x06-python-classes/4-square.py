1f478c64ea4355c636bef571a56ec8988d3cc8bd
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-21 16:59:18 +0100

    edit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
-    """Defines a square"""
-    def __init__(self, size=0):
-        """Initialises the data"""
-        self.size = size
-
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
-
-    @property
-    def size(self):
-        """Getter method"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Setter method"""
-        self.__size = value
-        if type(value) != int:
+    """Private instance attribute"""
+    def __init__(self, size=0) -> None:
+        self.__size = size
+        """if else statment"""
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        """area function with parameter(self)"""
+    def area(self):
+        """return area value"""
+        return self.__size ** 2
+
+    def size(self):
+        return self.__size
+    """size function"""
+    def size(self, value):
+        """if else statment"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
