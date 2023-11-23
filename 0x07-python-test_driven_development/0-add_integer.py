2aefc7ac38a2e51acb939fd4beaf1a9793b9b5b2
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-24 00:11:32 +0100

    edit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,12 @@ You are not allowed to import any module
 
 """
 def add_integer(a, b=98):
-    if not (isinstance(a, int) or isinstance(a, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    elif not (isinstance(b, int) or isinstance(b, float)):
+    elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + b
+    return int(a) + int(b)
+
+if __name__ =="__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
