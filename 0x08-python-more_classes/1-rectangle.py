1747a30488df5f26012f9eba4d64ea8354f45f01
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-27 19:26:37 +0100

    fix pycodestyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,27 +8,31 @@ class Rectangle:
         """initializes width and height"""
         self.height = height
         self.width = width
+
     @property
     def width(self):
         """Private instance attribute: width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """Laws of rectangle width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0 :
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """Private instance attribute: height"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Laws of rectangle height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0 :
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
