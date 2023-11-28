196ff495f19db1ca703bc385aaa78fa298c77b15
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-28 11:46:36 +0100

    add

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_square(size):
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    print((('#' * size + "\n") * size), end='')
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
