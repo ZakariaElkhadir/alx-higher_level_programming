76c67a1d99f955dac0a78695e1dfae3c0e126305
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-05 14:28:45 +0100

    edit some code and pycodestyle fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""write file method"""
 def write_file(filename="", text=""):
+    """read file with utf-8"""
     with open(filename,'w', encoding="utf-8", ) as file:
-        file.write(text)
+        return file.write(text)
