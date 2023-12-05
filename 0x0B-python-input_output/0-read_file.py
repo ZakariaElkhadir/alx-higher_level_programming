c9efc704e99a421e8ebb1e520e150ece5b9bc450
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-05 14:11:52 +0100

    edit some code in the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """function that read file content"""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
