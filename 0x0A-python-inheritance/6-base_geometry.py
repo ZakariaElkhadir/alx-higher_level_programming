86440c2f6d0c68e5ae52794d8a7153f4bbab9e86
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-06 17:56:51 +0100

    add file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class method"""
+
+
+class BaseGeometry:
+    """public instance method"""
+    def area(self):
+        """raise an exeption"""
+        raise Exception("area() is not implemented")
