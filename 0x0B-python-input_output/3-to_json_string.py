358c85f6f7b4610ab95c2077a8c3eb1af58a8cd6
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-06 01:16:01 +0100

    add file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
-#!/usr/bin/pyhton3
-"""define json string fun"""
-
-
+#!/usr/bin/python3
+"""This module defines a string-to-JSON function"""
 import json
-    
+
+
 def to_json_string(my_obj):
-    """returns json represintation of an object"""
+    """Returns the JSON representation of a string object"""
     return json.dumps(my_obj)
