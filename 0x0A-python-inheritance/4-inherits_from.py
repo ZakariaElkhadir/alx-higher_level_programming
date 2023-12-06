fcb946529245a002a52fc4e1741f59e1cd102a82
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-06 17:46:41 +0100

    edit some code and fixing pcs

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
-    else:
-        return False
+
+    return False
