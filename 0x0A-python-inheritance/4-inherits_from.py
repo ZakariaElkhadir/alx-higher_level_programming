be17ba139db855493b65aa7d6dcdcf9a0543393a
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-06 17:41:17 +0100

    add file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
