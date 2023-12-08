6186642136545b796bd60adf8c8d5d685c271bf9
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-08 16:01:48 +0100

    add area function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,63 @@ from models.base import Base
 class Rectangle(Base):
     """Class constructot"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """Rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validation("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validation("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Rectangle x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Rectangle y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validation("y", value)
+        self.__y = value
+
+    def validation(self, name, value, eq=True):
+        "Value validation method"
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        
+    def area(self):
+        """area method"""
+        return self.height * self.width
+
