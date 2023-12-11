554da1c01ed7e44dc3a1abd30052a60ca5924486
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-11 23:50:59 +0100

    fix pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Class constructot"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
