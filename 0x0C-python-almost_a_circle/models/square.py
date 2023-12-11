35859818bf19179086628642dc64ac1278f45022
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-11 23:59:46 +0100

    fix pycode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
+"""Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''A Square class.'''
+    """square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        '''Constructor.'''
-        super().__init__(size, size, x, y, id)
+        """Class constructor"""
+        super().__init__(size, size,  x, y, id)
 
     def __str__(self):
-        '''Returns string info about this square.'''
+        """ str function """
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        '''Size of this square.'''
         return self.width
 
     @size.setter
     def size(self, value):
-        self.width = value
+        self.witdh = value
         self.height = value
 
     def __update(self, id=None, size=None, x=None, y=None):
@@ -44,6 +43,5 @@ class Square(Rectangle):
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
-        return {"id": self.id, "size": self.width,
-                "x": self.x, "y": self.y}
+        """returns the dictionary representation of a Square:"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
