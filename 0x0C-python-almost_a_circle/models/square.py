b764d1b4e927d4a4f207788cdc6b2eec189657bb
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-11 23:54:55 +0100

    add

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+'''Module for Square class.'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square class"""
+    '''A Square class.'''
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Class constructor"""
-        super().__init__(size, size,  x, y, id)
+        '''Constructor.'''
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ str function """
+        '''Returns string info about this square.'''
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
+        '''Size of this square.'''
         return self.width
 
     @size.setter
     def size(self, value):
-        self.witdh = value
+        self.width = value
         self.height = value
 
     def __update(self, id=None, size=None, x=None, y=None):
@@ -42,5 +44,6 @@ class Square(Rectangle):
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        """returns the dictionary representation of a Square:"""
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
