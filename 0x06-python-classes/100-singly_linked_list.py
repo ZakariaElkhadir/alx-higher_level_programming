3aad02ef5434134a7cf4eeb25dc5c1b786ed11be
Parrot Live user <user@localhost.localdomain>
2023-12-29 22:21:13 +0000

    edit file

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,37 +1,49 @@
 #!/usr/bin/python3
-class Node:
-    def __init__(self, data, next_node=None):
+"""node class object"""
 
-        self.data = data
-        self.next_node = next_node
+
+class Node:
+
+
+    def __init__(self ,data ,next_node=None):
+
+       self.data = data
+       self.next_node = next_node
 
     @property
     def data(self):
         return self.__data
+
     @data.setter
     def data(self, value):
-
+    """initialize two object"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.__data = value
+        selInitializes an empty singly linked list.Initializes an empty singly linked list.f.__data = value
 
     @property
     def next_node(self):
-        """The next_node property."""
+    """Getter property for the data value of the node."""
+
         return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
+        """next_node setter"""
         if not isinstance(value, (Node, type(None))):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
-class SinglyLinkedList:
-    def __init__(self):
 
+class SinglyLinkedList:
+"""SinglyLinkedList class object"""
+
+    def __init__(self):
+    """initialize object"""
         self.__head = None
 
     def __str__(self):
-
+        """Initializes an empty singly linked list."""
         new_list = []
         currentNode = self.__head
         while currentNode:
@@ -40,7 +52,7 @@ class SinglyLinkedList:
         return "\n".join(str(z) for z in sorted(new_list))
 
     def sorted_insert(self, value):
+        """Inserts a new node with the given value in sorted order."""
 
         newNode = Node(value, self.__head)
         self.__head = newNode
-
