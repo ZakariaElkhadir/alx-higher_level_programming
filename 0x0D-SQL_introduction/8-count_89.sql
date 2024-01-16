968b8ca654816794abdb9525b7cc431de2563242
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-17 00:08:41 +0100

    add file

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table first_table
+-- -- cat 8-count_89.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+SELECT COUNT(8) FROM first_table WHERE id = 89;
