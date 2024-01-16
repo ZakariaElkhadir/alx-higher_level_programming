4cd7c1d05b4eb8af2714dbaddb0ca883343ee73b
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-16 23:11:36 +0100

    add file

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all the tables
+-- cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create table
+-- cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT, 
+    name VARCHAR(256)
+);
