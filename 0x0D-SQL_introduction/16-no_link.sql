68b4b3bbe5c2b85700cb5e1658c0df74025d6531
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-17 01:45:59 +0100

    add file

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- list all records
+-- cat 16-no_link.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table
+    WHERE name IS NOT NULL
+        ORDER BY 
+            score DESC;
