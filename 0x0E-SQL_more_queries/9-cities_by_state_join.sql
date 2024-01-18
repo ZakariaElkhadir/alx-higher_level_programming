582a4442ae5657b1ffc3913d4f0c364398528887
zakaria <zakaria@zakarias-MacBook-Pro.local>
2024-01-18 19:51:13 +0100

    add file

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- echo 'SELECT * FROM states;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- echo 'SELECT * FROM cities;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
--- cat 9-cities_by_state_join.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_usaSELECT c.id, c.name, s.name FROM cities c
+-- cat 9-cities_by_state_join.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_usaSELECT c.id, c.name, s.name
+FROM cities c
 JOIN states s ON c.state_id = s.id
 ORDER BY c.id ASC;
