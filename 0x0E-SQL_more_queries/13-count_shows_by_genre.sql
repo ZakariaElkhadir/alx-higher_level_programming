52450bac6036f5750177a2be4c788f930dcf1da6
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-18 13:25:15 -0800

    add file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,13 @@
 --lists all genres from hbtn_0d_tvshows
 -- cat 13-count_shows_by_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 
-SELECT c.name AS genre, COUNT(*) AS number_of_shows FROM tb_genres c
-	JOIN tv_show_genres ON c.id = s.genre_id
-GROUP BY c.name
-ORDER BY number_of_shows DESC;
+SELECT
+    c.name AS genre,
+    COUNT(*) AS number_of_shows
+FROM
+    tv_genres c
+    JOIN tv_show_genres s ON c.id = s.genre_id
+GROUP BY
+    c.name
+ORDER BY
+    number_of_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,10 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
 -- cat 14-my_genres.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 
-SELECT g.name AS name FROM tv_shows s
-LEFT JOIN tv_show_genres sg ON sg.genre_id = g.id
+SELECT g.name AS name
+FROM tv_shows s
 LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
 WHERE s.title = 'Dexter'
 ORDER BY name ASC;
 
