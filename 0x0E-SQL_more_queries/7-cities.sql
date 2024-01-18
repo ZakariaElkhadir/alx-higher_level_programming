603ac7cd04a571cdb3362b637b592eb0e3b0eafd
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-18 01:57:09 +0100

    add file

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,12 @@
 -- create database and table
 -- cat 7-cities.sql | mysql -hlocalhost -uroot -p
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE cities (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states (id)
+
+USE hbtn_0d_usa
+CREATE TABLE
+    IF NOT EXISTS cities (
+        id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+        state_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        FOREIGN KEY (state_id) REFERENCES states (id)
 )
