4279b515d2267551f916ffccd9707e69450d12de
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-13 00:29:01 +0100

    edit file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,9 @@ const { argv } = require('process');
 const char = 'X';
 
 const size = parseInt(argv[2], 10);
-let i = 0;
+
 if (!isNaN(size)) {
-  for (; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     console.log(char.repeat(size));
   }
 } else console.log('Missing size');
