f1a80756731e5dc26867146149f3840d7fb5fe25
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-12 19:30:39 +0100

    add file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,4 +5,5 @@ const { argv } = require('node:process');
 // print process.argv
 
 if (argv.length <= 2) console.log('No argument');
-if (argv.length === 3 || argv.length > 3) console.log('Argument found');
+if (argv.length === 3) console.log('Argument found');
+if (argv.length > 3) console.log('Argument found');
