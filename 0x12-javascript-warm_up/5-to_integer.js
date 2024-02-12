a45d0929e56b86f8a9789e91c152348a69bf74df
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-12 23:00:54 +0100

    add file

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,6 +3,5 @@
 const { argv } = require('process');
 const myarg = Number(argv[2]);
 
-if (typeof myarg === 'number') console.log(Math.floor(myarg));
-if (typeof myarg !== 'number') console.log('Not a number')
-
+if (!isNaN(parseInt(myarg, 10))) console.log('My number: ' + parseInt(myarg, 10));
+else console.log('Not a number');
