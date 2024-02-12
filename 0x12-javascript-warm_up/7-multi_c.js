471a984e4f174c8d416eba9b45e449e5f35e366d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-12 23:40:30 +0100

    add file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,13 +2,13 @@
 
 const { argv } = require('process');
 
-const string = 'C is fun'
+const string = 'C is fun';
 
 let i = parseInt(argv[2], 10);
-if (!isNaN(i)){
-for (; i > 0; i--){
-    console.log(string)
-}
-}else {
-    console.log('Missing number of occurrences')
+if (!isNaN(i)) {
+  for (; i > 0; i--) {
+    console.log(string);
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
