c43a3a3b05648272505d2f2cfa0d419f8fc9d191
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-13 19:50:28 +0100

    add file

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+
+  this.width = w;
+  this.width = h;
+  if (w <= 0 || h <= 0){
+      const obj = {
+
+      }
+    }
+}
+}
+
+
+module.exports = Rectangle;
