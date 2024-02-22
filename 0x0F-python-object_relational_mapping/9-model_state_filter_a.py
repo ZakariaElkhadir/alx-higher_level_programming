2949bf3656ce3f051b5f061252f339a7126fb687
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-22 19:36:32 +0100

    add file

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_five = session.query(State).order_by(State.id).limit(5).all()
+
+    for state in states_five:
+        print(f"{state.id}: {state.name}")
+    session.close()
