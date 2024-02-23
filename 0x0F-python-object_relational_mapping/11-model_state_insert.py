991b8fbfe5291593210f5f94d69ca7a4d92736a8
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-24 00:34:44 +0100

    edit file

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,7 +11,8 @@ username = sys.argv[1]
 password = sys.argv[2]
 db_name = sys.argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, db_name),pool_pre_ping=True)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    username, password, db_name), pool_pre_ping=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
