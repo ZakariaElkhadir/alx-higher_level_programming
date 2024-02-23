44526e99ff11bf811eb6520ff953b99a7890e33e
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-23 23:48:31 +0100

    edit file

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).filter(
+        State.name == state_name).order_by(State.id).first()
+
+    if states:
+        print(states.id)
+    else:
+        print("Nor found")
+
+    session.close()
