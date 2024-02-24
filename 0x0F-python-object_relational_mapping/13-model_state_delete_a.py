c9c004dff0d458a174285cf0fd67634802f5cd00
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-24 01:03:02 +0100

    edit file

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
+
 """
 database model
 """
@@ -26,5 +26,5 @@ if __name__ == "__main__":
     for state in states_delete:
         session.delete(state)
     session.commit()
-        
+
     session.close()
