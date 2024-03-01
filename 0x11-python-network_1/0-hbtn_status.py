37d1dadfaef92da366d7d1bb75055b2e28ad4e20
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-01 23:30:55 +0100

    add file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html_content = response.read().decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(html_content)))
+        print("\t- content: {}".format(html_content))
+        print("\t- utf8 content: {}".format(html_content))
