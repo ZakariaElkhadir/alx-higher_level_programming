083084bf892b9376ea34991c6c3138361c4c59f9
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-02 00:55:51 +0100

    Refactor code to improve performance and readability

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+doc doc
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get("X-Request-Id")
+        print(f"{x_request_id}")
+
