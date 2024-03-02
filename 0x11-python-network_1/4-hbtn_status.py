61d3611c3acbe7512777008dd7cd2746b07225df
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-02 02:19:44 +0100

    edit file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ script that fetches link
 """
 import requests
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     request = requests.get(url)
     cont = request.text
     print("Body response:")
