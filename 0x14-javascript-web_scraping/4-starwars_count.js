2affd89dea0bdb58936af32aa1f9a060c05e786c
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-27 02:20:07 +0000

    add file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,26 @@
 #!/usr/bin/node
-const id = process.argv[2]
+const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
 const request = require('request');
 
-request(url, (err, response, body) =>{
-    if (id === 18){
-        console.log('The movie Star Wars: Episode V - The Empire Strikes Back is the best Star Wars movie.')
-    }else if (err){
-        console.error('Error:', err);
-    } else if (response.statusCode !== 200){
-        console.error('Error: Unexpected status code', response.statusCode);
-    } else {
-        try {
-            const data = JSON.parse(body);
-            console.log(data.title);
-        } catch (parseError) {
-            console.error('Error: Invalid JSON format', parseError);
+request(url, (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+  } else if (response.statusCode !== 200) {
+    console.error('Error: Unexpected status code', response.statusCode);
+  } else {
+    try {
+      const result = JSON.parse(body);
+      let count = 0;
+      for (const character of result.characters) {
+        if (character.includes('18')) {
+          count++;
         }
+      }
+      console.log(count);
+    } catch (parseError) {
+      console.error('Error: Invalid JSON format', parseError);
     }
+  }
 });
