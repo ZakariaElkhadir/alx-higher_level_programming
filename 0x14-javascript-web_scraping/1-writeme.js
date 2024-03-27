9c11351abb6d942103aeb0dfa5e97c45e55d3499
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-27 01:26:30 +0000

    edit file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(file, text, 'utf-8', function (err) {
-  console.log(err);
+fs.writeFile(file, text, 'utf-8', function (err, data = err) {
+  console.log(data);
 });
